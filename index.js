b0117b0469bf9932c5a4392f276473d69747dcd6
Mike Papesca <michael.papesca@trove.co>
2022-12-02 10:44:41 -0700

    Add check to prevent future days or NAN args

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const dayNumber = process.argv[2];
-if(dayNumber === undefined) {
-  console.log('please provide a day to run');
+
+if (dayNumber === undefined || isNaN(dayNumber)) {
+  console.log('PLEASE PROVIDE A NUMERIC DAY TO RUN');
   return;
 }
 
-const { run } = require(`./day-${dayNumber}`);
+let run;
+try {
+  const { run: dayRun } = require(`./day-${dayNumber}`);
+  run = dayRun;
+} catch (error) {
+  console.log(`DAY ${dayNumber} DOES NOT EXIST YET`);
+  process.exit();
+}
+
 console.log(`STARTING DAY ${dayNumber}`);
 run()
   .then(result => console.log({result}))
